8f1d3989979eba50f487a489500724ac9868f4b5
phamtuankhoinodejs <huynhanhpham734@gmail.com>
2022-08-08 14:37:24 +0700

    CRUD module bages

--- src/badges/badges.module.ts
+++ src/badges/badges.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BadgesService } from './badges.service';
 import { BadgesController } from './badges.controller';
+import { Badges } from './schema/badges.schema';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
+  imports: [TypegooseModule.forFeature([Badges])],
   controllers: [BadgesController],
   providers: [BadgesService]
 })
