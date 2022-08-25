5bdf643f834ece461a2534ffea44b02878932827
phamtuankhoinodejs <huynhanhpham734@gmail.com>
2022-08-25 16:32:45 +0700

    refresh password

--- src/user/dtos/update-user.dto.ts
+++ src/user/dtos/update-user.dto.ts
@@ -1,10 +1,85 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator';
 import { RegisterUserDto } from './register-user.dto';
 
-export class UpdateUserDto extends PartialType(RegisterUserDto) {
+export class UpdateUserDto {
   @IsOptional()
   @IsString()
   @IsEmail()
   emailOld: string;
+
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  cover: string;
+
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  avatar: string;
+
+  @IsOptional()
+  @IsEmail()
+  @Type(() => String)
+  email: string;
+
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  @Length(2, 100)
+  username: string;
+
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  @Length(4, 30)
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  @Length(4, 30)
+  confirmPassword: string;
+
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  @Length(2, 30)
+  displayName: string;
+
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  bio: string;
+
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  customUrl: string;
+
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  facebook: string;
+
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  twitter: string;
+
+  @IsOptional()
+  @IsString()
+  @Type(() => String)
+  discord: string;
+
+  @IsOptional()
+  @IsNumber()
+  power: number;
 }
